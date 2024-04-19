f2fc15ae3da8d810d67885e3f22914fc86312d69
Mintype <900raiyan@gmail.com>
2024-04-19 14:57:39 -0400

    added /status

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, Request, Response } from 'express';
 
 const app: Application = express();
 
+// Middleware to check server status
+app.use('/status', (req: Request, res: Response) => {
+    res.status(200).json({ status: 'Server is running' });
+});
+
+// Default route
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!');
 });
 
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
