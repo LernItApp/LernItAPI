86dc39a48aba971e73ed06e2ae2ec2811e78efe7
Mintype <900raiyan@gmail.com>
2024-04-19 14:53:39 -0400

    Create Typescript project

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+});
