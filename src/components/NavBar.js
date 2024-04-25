8d44e88708d080e1b3d028a8808fca6406cc17a1
Mintype <900raiyan@gmail.com>
2024-04-25 08:37:01 -0400

    Add code to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <div>
+        <ul className="navbar">
+            <li className="title" id="nav-item"><a href="/">LernIt</a></li>
+            <li className="nav-item" id="nav-item"><a href="/mysets">My Sets</a></li>
+            <li className="nav-item" id="nav-item"><a href="/new">New</a></li>
+            <li className="nav-item" id="nav-item"><a href="/about">About</a></li>
+            <li className="nav-item" id="nav-item"><a href="/me">Me</a></li>
+        </ul>
+    </div>
   )
 }
 
