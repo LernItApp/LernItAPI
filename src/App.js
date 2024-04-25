b24f75b56af55d090f8ca0239f194efbab7298a5
Mintype <900raiyan@gmail.com>
2024-04-24 23:34:40 -0400

    Created SignUp page.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home.js";
 import Login from "./pages/Login.js";
+import SignUp from "./pages/SignUp.js";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route exact  path="/" element={<Home />} />
         <Route exact path="/login" element={<Login />} />
+        <Route exact path="/signup" element={<SignUp />} />
       </Routes>
     </Router>
   );
