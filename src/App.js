600a4244d1c544fed8d433eed6e1099d0263f8da
Mintype <900raiyan@gmail.com>
2024-04-25 08:14:57 -0400

    Added routes for privacy, contact, and termsofservice.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Home from "./pages/Home.js";
 import Login from "./pages/Login.js";
 import SignUp from "./pages/SignUp.js";
 import CreateStudyList from "./pages/CreateStudyList.js";
+import TermsOfService from "./pages/TermsOfService.js";
+import Contact from "./pages/Contact.js";
+import Privacy from "./pages/Privacy.js";
 
 function App() {
   return (
@@ -14,6 +17,9 @@ function App() {
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/signup" element={<SignUp />} />
         <Route exact path="/create-study-list" element={<CreateStudyList />} />
+        <Route exact path="/privacy" element={<Privacy />} />
+        <Route exact path="/contact" element={<Contact />} />
+        <Route exact path="/termsofservice" element={<TermsOfService />} />
       </Routes>
     </Router>
   );
