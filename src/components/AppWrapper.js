01b61c64954b3391dae723cadfe2369cca9b594f
Mintype <900raiyan@gmail.com>
2024-04-25 08:05:40 -0400

    Added Footer

--- src/components/AppWrapper.js
+++ src/components/AppWrapper.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 function AppWrapper() {
   return (
-    <div>AppWrapper</div>
+    <div>
+        <footer>
+            <p>Copyright © 2024 LernItApp. All rights reserved.</p>
+            <p><a href="/terms">Terms of Service</a> | <a href="/privacy">Privacy Policy</a> | <a href="/contact">Contact Us</a></p>
+        </footer>
+    </div>
   )
 }
 
