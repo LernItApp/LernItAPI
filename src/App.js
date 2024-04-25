d1f0d0e53d48fe1261b4ae8c170a648ccb8a5631
Mintype <900raiyan@gmail.com>
2024-04-25 08:26:10 -0400

    Implement AppWrapper

--- src/App.js
+++ src/App.js
@@ -8,20 +8,23 @@ import CreateStudyList from "./pages/CreateStudyList.js";
 import TermsOfService from "./pages/TermsOfService.js";
 import Contact from "./pages/Contact.js";
 import Privacy from "./pages/Privacy.js";
+import AppWrapper from "./components/AppWrapper.js";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact  path="/" element={<Home />} />
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/signup" element={<SignUp />} />
-        <Route exact path="/create-study-list" element={<CreateStudyList />} />
-        <Route exact path="/privacy" element={<Privacy />} />
-        <Route exact path="/contact" element={<Contact />} />
-        <Route exact path="/termsofservice" element={<TermsOfService />} />
-      </Routes>
-    </Router>
+    <AppWrapper>
+      <Router>
+        <Routes>
+          <Route exact  path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signup" element={<SignUp />} />
+          <Route exact path="/create-study-list" element={<CreateStudyList />} />
+          <Route exact path="/privacy" element={<Privacy />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/termsofservice" element={<TermsOfService />} />
+        </Routes>
+      </Router>
+    </AppWrapper>
   );
 }
 
--- src/components/AppWrapper.js
+++ src/components/AppWrapper.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import NavBar from './NavBar'
 
-function AppWrapper() {
+function AppWrapper({ children }) {
   return (
     <div>
+      <NavBar/>
+      <div className="app-container">{children}</div>
         <footer>
             <p>Copyright © 2024 LernItApp. All rights reserved.</p>
             <p><a href="/terms">Terms of Service</a> | <a href="/privacy">Privacy Policy</a> | <a href="/contact">Contact Us</a></p>
